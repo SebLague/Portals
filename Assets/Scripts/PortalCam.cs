9c46d78b8de045893a5f0ea0af606aa38bac81dc
Sebastian Lague <sebastlague@gmail.com>
2020-01-05 16:06:10 +0200

    Made two portals. Not working.

--- Assets/Scripts/PortalCam.cs
+++ Assets/Scripts/PortalCam.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PortalCam : MonoBehaviour {
 
-    public Transform portal;
+    public Transform myPortal;
+    public Transform otherPortal;
     Camera mainCam;
 
     void Start () {
@@ -12,8 +13,19 @@ public class PortalCam : MonoBehaviour {
     }
 
     void LateUpdate () {
-        transform.position= mainCam.transform.position;
-        transform.rotation= mainCam.transform.rotation;
-        
+        Vector3 linkedPortalOffset = (otherPortal.position - mainCam.transform.position);
+
+        Plane portalPlane = new Plane (otherPortal.forward, otherPortal.position);
+
+        Ray mainCamRay = new Ray (mainCam.transform.position, mainCam.transform.forward);
+        float dstToPortalPlane;
+        portalPlane.Raycast (mainCamRay, out dstToPortalPlane);
+
+        Vector3 p = mainCamRay.GetPoint (dstToPortalPlane);
+        Debug.DrawRay (p, mainCamRay.direction, Color.red);
+
+        transform.position = otherPortal.position;
+        transform.rotation = mainCam.transform.rotation;
+
     }
 }
