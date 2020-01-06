516698cd39666efececcc6ff13da237f7bca73ab
Sebastian Lague <sebastlague@gmail.com>
2020-01-06 15:26:08 +0200

    Detect player going through portal

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Vector3 posOld;
+
+    public BoxCollider portalCollider;
+
+    void Start () {
+        posOld = transform.position;
+    }
+
+    void Update () {
+        Vector3 posNew = transform.position;
+        Plane plane = new Plane (portalCollider.transform.forward, portalCollider.transform.position);
+        float colliderDepth = portalCollider.size.z;
+
+        if (!plane.SameSide (posOld, posNew)) {
+            float dstTravelled = (posNew - posOld).magnitude;
+            Vector3 dir = (posNew - posOld) / dstTravelled;
+
+            if (portalCollider.Raycast (new Ray (posOld - dir * colliderDepth, dir), out _, dstTravelled + colliderDepth)) {
+                Debug.Log ("Went through portal!");
+            } else {
+                Debug.Log ("Went passed portal");
+            }
+        }
+        posOld = posNew;
+    }
+}
