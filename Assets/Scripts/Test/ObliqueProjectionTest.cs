7f8ee45abbd42250579d59c775a8fb0b4ff34d53
Sebastian Lague <sebastlague@gmail.com>
2020-01-09 15:55:44 +0200

    oblique both dir test (seems to be working...)

--- Assets/Scripts/Test/ObliqueProjectionTest.cs
+++ Assets/Scripts/Test/ObliqueProjectionTest.cs
@@ -6,19 +6,21 @@ public class ObliqueProjectionTest : MonoBehaviour {
 
     public Transform plane;
 
-    void Start () {
+    void LateUpdate () {
 
         // From http://tomhulton.blogspot.com/2015/08/portal-rendering-with-offscreen-render.html
         if (plane) {
             Camera cam = Camera.main;
 
+            int dot = (Vector3.Dot (cam.transform.forward, plane.forward) < 0) ? -1 : 1;
+            print ("dot: " + dot);
             // Calculate clip plane for portal (for culling of objects in-between destination camera and portal)
             Vector4 clipPlaneWorldSpace =
                 new Vector4 (
-                    plane.forward.x,
-                    plane.forward.y,
-                    plane.forward.z,
-                    Vector3.Dot (plane.position, -plane.forward));
+                    plane.forward.x * dot,
+                    plane.forward.y * dot,
+                    plane.forward.z * dot,
+                    Vector3.Dot (plane.position, -plane.forward * dot));
 
             Vector4 clipPlaneCameraSpace =
                 Matrix4x4.Transpose (Matrix4x4.Inverse (cam.worldToCameraMatrix)) * clipPlaneWorldSpace;
