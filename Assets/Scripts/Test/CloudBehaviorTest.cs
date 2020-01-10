a2fb9b104334917c7873febf4c6e20e71865cbe0
Sebastian Lague <sebastlague@gmail.com>
2020-01-10 11:31:43 +0200

    Low poly cloud test

---
+++ Assets/Scripts/Test/CloudBehaviorTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviorTest : MonoBehaviour {
+    public Vector2 rotSpeedMinMax = new Vector2 (10, 20);
+    public float falloffDst = 3;
+    public float maxScale = 1;
+    float rotSpeed;
+
+    Transform cloudCentre;
+
+    void Start () {
+        rotSpeed = Random.Range (rotSpeedMinMax.x, rotSpeedMinMax.y);
+        cloudCentre = GameObject.FindGameObjectWithTag ("Player").transform;
+    }
+
+    void Update () {
+        transform.RotateAround (Vector3.zero, Vector3.up, Time.deltaTime * rotSpeed);
+        float sqrDst = (transform.position - cloudCentre.position).sqrMagnitude;
+        float t = 1 - Mathf.Min (1, sqrDst / (falloffDst * falloffDst));
+        transform.localScale = Vector3.one * t;
+    }
+}
---
+++ Assets/Scripts/Test/CloudTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudTest : MonoBehaviour {
+    public int numViewDirections = 100;
+    public float scale = 1;
+    public float spawnRadius = 10;
+    [Range (0, 1)]
+    public float startHeight;
+    public GameObject cloudPrefab;
+
+    void Start () {
+
+        float goldenRatio = (1 + Mathf.Sqrt (5)) / 2;
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < numViewDirections; i++) {
+            float t = (float) i / numViewDirections;
+            float inclination = Mathf.Acos (1 - (1 - startHeight) * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin (inclination) * Mathf.Sin (azimuth);
+            float y = Mathf.Cos (inclination);
+            float z = Mathf.Sin (inclination) * Mathf.Cos (azimuth);
+
+            var g = Instantiate (cloudPrefab, new Vector3 (x, y, z) * spawnRadius, Quaternion.identity);
+            g.transform.localScale = Vector3.one * scale;
+        }
+    }
+}
