85dfcf2a996160cb42e361d24a4a36dc2a77d309
Sebastian Lague <sebastlague@gmail.com>
2020-01-10 12:01:29 +0200

    Cloud test in forest scene (slow)

--- Assets/Scripts/Test/CloudTest.cs
+++ Assets/Scripts/Test/CloudTest.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CloudTest : MonoBehaviour {
     public int numViewDirections = 100;
     public int numClouds = 10;
-    public float scale = 1;
+    public int cloudSpawnSeed;
+    public bool randomizeCloudSeed;
+
     public float spawnRadius = 10;
     [Range (0, 1)]
     public float startHeight;
@@ -27,11 +29,15 @@ public class CloudTest : MonoBehaviour {
             float z = Mathf.Sin (inclination) * Mathf.Cos (azimuth);
 
             var g = Instantiate (cloudPrefab, new Vector3 (x, y, z) * spawnRadius, Quaternion.identity);
-            g.transform.localScale = Vector3.one * scale;
         }
 
+        if (randomizeCloudSeed) {
+            cloudSpawnSeed = Random.Range (-10000, 10000);
+        }
+        var prng = new System.Random (cloudSpawnSeed);
+
         for (int i = 0; i < numClouds; i++) {
-            float t = Random.value;
+            float t = (float) prng.NextDouble ();
             float inclination = Mathf.Acos (1 - (1 - startHeight) * t);
             float azimuth = angleIncrement * i;
 
