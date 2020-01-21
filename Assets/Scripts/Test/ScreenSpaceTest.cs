62dfd57e4ec3f8b509776f1abe1952eb616b2f3d
Sebastian Lague <sebastlague@gmail.com>
2020-01-21 18:17:50 +0200

    fixed screenpos test by dividing by w in the frag shader

--- Assets/Scripts/Test/ScreenSpaceTest.cs
+++ Assets/Scripts/Test/ScreenSpaceTest.cs
@@ -17,17 +17,19 @@ public class ScreenSpaceTest : MonoBehaviour {
         var b = cam.projectionMatrix * worldToCam * meshFilter.transform.localToWorldMatrix;
 
         var verts = meshFilter.mesh.vertices;
-        var uvs = new Vector2[verts.Length];
-
+        //var uvs = new Vector4[verts.Length];
+        var uvs = new List<Vector4> (verts.Length);
         // to projection space
         for (int i = 0; i < verts.Length; i++) {
 
             var projectionSpaceVert = b * new Vector4 (verts[i].x, verts[i].y, verts[i].z, 1);
 
-            projectionSpaceVert /= projectionSpaceVert.w;
-
-            uvs[i] = new Vector2 (projectionSpaceVert.x*.5f+.5f, projectionSpaceVert.y*.5f+.5f);
+            //projectionSpaceVert /= projectionSpaceVert.w;
+            //uvs[i] = projectionSpaceVert;
+            uvs.Add(projectionSpaceVert);
+            //uvs[i] = new Vector2 (projectionSpaceVert.x, projectionSpaceVert.y);
         }
-        meshFilter.mesh.uv = uvs;
+        //meshFilter.mesh.uv = uvs;
+        meshFilter.mesh.SetUVs (0, uvs);
     }
 }
