d5a9024b2e89f66cd4650097e5d7a78a46470349
Sebastian Lague <sebastlague@gmail.com>
2020-01-28 00:41:26 +0200

    Fixed jerkiness when turning player through angled portals

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -95,10 +95,9 @@ public class FPSController : PortalTraveller {
         float mX = Input.GetAxisRaw ("Mouse X");
         float mY = Input.GetAxisRaw ("Mouse Y");
 
+        // Verrrrrry gross hack to stop camera swinging down at start
         float mMag = Mathf.Sqrt (mX * mX + mY * mY);
-        //Debug.Log (mMag);
         if (mMag > 5) {
-
             mX = 0;
             mY = 0;
         }
@@ -106,8 +105,8 @@ public class FPSController : PortalTraveller {
         yaw += mX * mouseSensitivity;
         pitch -= mY * mouseSensitivity;
         pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
-        smoothPitch = Mathf.SmoothDamp (smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
-        smoothYaw = Mathf.SmoothDamp (smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+        smoothPitch = Mathf.SmoothDampAngle (smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle (smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
 
         transform.eulerAngles = Vector3.up * smoothYaw;
         cam.transform.localEulerAngles = Vector3.right * smoothPitch;
@@ -117,8 +116,9 @@ public class FPSController : PortalTraveller {
     public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
         transform.position = pos;
         Vector3 eulerRot = rot.eulerAngles;
-        yaw = eulerRot.y;
-        smoothYaw = yaw;
+        float delta = Mathf.DeltaAngle (smoothYaw, eulerRot.y);
+        yaw += delta;
+        smoothYaw += delta;
         transform.eulerAngles = Vector3.up * smoothYaw;
         velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (velocity));
         Physics.SyncTransforms ();
